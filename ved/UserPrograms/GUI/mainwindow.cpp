000f629e127a2b3bfbc8dc8578926ca307383240
Zeerog <55512675+Zeerog@users.noreply.github.com>
2019-11-01 03:18:23 +0200

    Minor changes in GUI

--- ved/UserPrograms/GUI/mainwindow.cpp
+++ ved/UserPrograms/GUI/mainwindow.cpp
@@ -1,17 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "QFileDialog"
+#include "createnew.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QStringList Header;
-    Header<<"Активность"<<"Диск"<<"Размер"<<"Путь";
-    ui->tableWidget->setColumnCount(4);
-    ui->tableWidget->setHorizontalHeaderLabels(Header);
 }
-
 
 MainWindow::~MainWindow()
 {
@@ -19,39 +14,10 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_CreateNew_clicked()
+void MainWindow::on_pushButton_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(1);
-}
-
-void MainWindow::on_buttonBox_accepted()
-{
-    int NewRow;
-    ui->tableWidget->insertRow(ui->tableWidget->rowCount());
-    NewRow=ui->tableWidget->rowCount()-1;
-    ui->tableWidget->setItem(NewRow,0,new QTableWidgetItem("0"));
-    ui->tableWidget->setItem(NewRow,1,new QTableWidgetItem(ui->DiskLetter->text()));
-    ui->tableWidget->setItem(NewRow,2,new QTableWidgetItem(ui->DiskSize->text()));
-    ui->tableWidget->setItem(NewRow,3,new QTableWidgetItem(ui->Path->text()));
-    ui->stackedWidget->setCurrentIndex(0);
-}
-
-void MainWindow::on_buttonBox_rejected()
-{
-    ui->stackedWidget->setCurrentIndex(0);
-}
-
-void MainWindow::on_PathButton_clicked()
-{
-    ui->Path->setText(QFileDialog::getExistingDirectory(this, "Get Any File","C://"));
-}
-
-void MainWindow::on_Add_clicked()
-{
-
-}
-
-void MainWindow::on_Remove_clicked()
-{
-    ui->tableWidget->removeRow(ui->tableWidget->currentRow());
+    CreateNew NewDisk;
+    NewDisk.setFixedSize(500,220);
+    NewDisk.setModal(true);
+    NewDisk.exec();
 }
